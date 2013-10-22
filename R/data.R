6bc9d1926be6b5c63d704d6f1bcefed14e7173c7
François Guillem <guillem_francois@yahoo.fr>
2013-10-22 22:06:17 +0200

    Amélioration prSave et prLoad: date de création, masquer les détails, erreurs

--- R/data.R
+++ R/data.R
@@ -1,6 +1,10 @@
 prSave <- function(name, replace = FALSE, desc = "No description", subdir) {
   if (!is.character(name) || length(name) > 1) stop("Argument 'name' should be a character vector of length one. Have you forgotten quotes ?")
   
+  if(!file.exists("data")) {
+    stop("Directory 'data' does not exist. Have you initialized the project with prInit ?")
+  }
+  
   if (missing(subdir)) {
     file <- sprintf("data/%s.rda", name)
   } else {
@@ -9,15 +13,33 @@ prSave <- function(name, replace = FALSE, desc = "No description", subdir) {
   if(!replace & file.exists(file)) {
     stop("File already exists. Use 'replace=TRUE' if you want to overwrite.")
   }
-  eval(parse(text = prFormat("attr(#{name}, 'desc') <- desc")))
+  eval(parse(text = sprintf("attr(%s, '._desc') <- desc", name)))
+  eval(parse(text = sprintf("attr(%s, '._creationTime') <- Sys.time()", name)))
   save(list = name, file = file)
 }
 
-prLoad <- function(name, project= ".") {
-  file <- sprintf("%s/data/%s.rda", 
-                   project,
-                   name)
+prLoad <- function(name, trace = TRUE, description = TRUE, creationTime = TRUE) {
+  file <- sprintf("data/%s.rda", name)
   load(file, envir=.GlobalEnv)
-  cat(sprintf("%s '%s' has been loaded :\n", class(get(name))[1], name))
-  cat("   ", attr(get(name), "desc"), "\n")
+  if (trace) {
+    if (class(get(name))[1] %in% c("numeric", "integer", "character", "logical")) {
+      objClass <- paste(class(get(name))[1], 
+                        ifelse(length(get(name)) == 1, "value", "vector"))
+    } else {
+      objClass <- class(get(name))[1]
+    }
+    objClass <- gsub("^(.)", "\\U\\1", objClass, perl = T)
+    
+    cat(sprintf("%s '%s' has been loaded%s%s\n", 
+                objClass, 
+                name,
+                ifelse(creationTime, 
+                       sprintf(" (saved on %s)", attr(get(name), "._creationTime")),
+                       ""),
+                ifelse(description, ":", ".")))
+    
+    if(description) {
+      cat("   ", attr(get(name), "._desc"), "\n")
+    }
+  }
 }
