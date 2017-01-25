900caeb7baa3628318bb46e63d91d0e7b63bd786
cuche27 <guillem_francois@yahoo.fr>
2017-01-25 13:08:54 +0100

    New function prPath()

--- R/helpers.R
+++ R/helpers.R
@@ -28,34 +28,14 @@
   file.remove(.getPath(name, subdir, extension, mainDir, create = FALSE))
 }
 
-
-# Get subdirectory of an object and create it if it does not exist
-.getPath <- function(name, subdir = ".", extension = NULL, mainDir = ".", 
-                     stopIfExists = FALSE, create = TRUE) {
-  if (is.null(getOption("projectRoot"))) {
-    stop("Project has not been initialized. Use 'prInit' to create a new project or 'prStart' to use an existing one")
-  }
-  subdir <- file.path(options("projectRoot"), mainDir, subdir, dirname(name))
-  subdir <- gsub("(/\\.)+/", "/", subdir)
-  subdir <- gsub("/\\.?$", "", subdir)
-  
-  if (!is.null(extension)) extension <- paste0(".", extension)
-  else extension <- ""
-  
-  path <- sprintf("%s/%s%s", subdir, basename(name), extension)
-  
-  if (stopIfExists & file.exists(path))
-    stop("File already exists. Use 'replace=TRUE' if you want to overwrite.")
-  
-  if (!dir.exists(subdir))  {
-    if (create) dir.create(subdir, recursive = TRUE)
-    else stop("Directory", subdir, "does not exist.")
-  }
-  
-  path
+#' Private function that returns the file name of a "substitute" expression.
+#' 
+#' Thanks to this function, quotes are optional in most functions of the package.
+#' 
+#' @param x result of the function "substitute"
+#' @returns character string
+#' @noRd
+.getName <- function(x) {
+  x <- deparse(x)
+  gsub('"|\'', "", x)
 }
-
-
-
-
-
---
+++ R/path.R
@@ -0,0 +1,49 @@
+#' Get the full path of a file
+#' 
+#' The function returns the absolute path of a file contained in the current
+#' project.
+#' 
+#' @param x Relative path of a file contained in the project.
+#' 
+#' @return 
+#' Absolute path of the file.
+#' 
+#' @examples 
+#' dontrun{
+#'   prPath("data")
+#' }
+#' 
+#' @export
+prPath <- function(x) {
+  x <- .getName(substitute(x))
+  .getPath(x, create = FALSE)
+}
+
+# Get subdirectory of an object and create it if it does not exist
+.getPath <- function(name, subdir = ".", extension = NULL, mainDir = ".", 
+                     stopIfExists = FALSE, create = TRUE) {
+  if (is.null(getOption("projectRoot"))) {
+    stop("Project has not been initialized. Use 'prInit' to create a new project or 'prStart' to use an existing one")
+  }
+  subdir <- file.path(options("projectRoot"), mainDir, subdir, dirname(name))
+  subdir <- gsub("(/\\.)+/", "/", subdir)
+  subdir <- gsub("/\\.?$", "", subdir)
+  
+  if (!is.null(extension)) extension <- paste0(".", extension)
+  else extension <- ""
+  
+  path <- sprintf("%s/%s%s", subdir, basename(name), extension)
+  
+  if (stopIfExists & file.exists(path))
+    stop("File already exists. Use 'replace=TRUE' if you want to overwrite.")
+  
+  if (!dir.exists(subdir))  {
+    if (create) dir.create(subdir, recursive = TRUE)
+    else stop("Directory", subdir, "does not exist.")
+  }
+  
+  path
+}
+
+
+
