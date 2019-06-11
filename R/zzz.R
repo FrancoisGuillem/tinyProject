6871f34dcfe6ec45d870876cc2fdd37ab0f32c26
cuche27 <guillem_francois@yahoo.fr>
2019-06-11 07:54:26 +0200

    Add missing import and update doc

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 #' @import grDevices
 #' @importFrom methods formalArgs
-#' @importFrom utils type.convert write.csv write.csv2 write.table
+#' @importFrom utils type.convert write.csv write.csv2 write.table packageDescription
 #' 
 NULL
 
