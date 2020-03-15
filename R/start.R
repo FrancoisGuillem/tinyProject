c8d77f38e45dddc950bc128884960bed6dc503af
gisma <reudenbach@uni-marburg.de>
2020-03-15 21:37:38 +0100

    fix some bugs

--- R/start.R
+++ R/start.R
@@ -19,6 +19,7 @@
 #' @export
 #'
 prStart <- function(dir = ".", trace = TRUE) {
+  options("projectRoot" = normalizePath(dir))
   oldProjectRoot <- getOption("projectRoot")
   options("projectRoot" = normalizePath(dir))
   ok <- FALSE
